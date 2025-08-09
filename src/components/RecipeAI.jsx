73360bf0f4ae218bf83e14bf3aa7f566d6be9a4e
zbrusco <194739391+zbrusco@users.noreply.github.com>
2025-08-09 20:25:58 -0300

    fix: update header text

--- src/components/RecipeAI.jsx
+++ src/components/RecipeAI.jsx
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 export default function RecipeAI(props) {
   return (
     <section className="ai-recipe-container" aria-live="polite">
-      <h2>Chef Claude Recommends:</h2>
+      <h2>Chef Gemini Recommends:</h2>
       <ReactMarkdown>{props.recipe}</ReactMarkdown>
     </section>
   );
